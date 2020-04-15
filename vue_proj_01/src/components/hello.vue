508b8babb346125c5294165485018e23086d839e
zhy <2282917871@qq.com>
2020-04-15 08:12:26 +0800

    2020/4/15

--- vue_proj_01/src/components/hello.vue
+++ vue_proj_01/src/components/hello.vue
@@ -1,87 +1,94 @@
 <template>
-    <div>
-        <h1>{{h1}}</h1>
-        <button @click="btn">点击</button>
-        <hr>
-         <label for="">
-            ID
-               <input type="text" v-model="id">
-        </label>
-        <label for="">
-            姓名
-               <input type="text" v-model="name">
-        </label>
-        <label for="">
-            年纪
-                  <input type="text" v-model="age">
-        </label>
-     
-  
-        <input type="button" value="添加" @click="btn1">
+  <div>
+    <label for>
+      名字:
+      <el-input v-model="name" placeholder="请输入名字" id="name" style="width:250px"></el-input>
+    </label>
 
-        <table border="1" id="table">
-            <thead>
-                 <tr>
-                <th>ID</th>
-                <th>姓名</th>
-                 <th>年龄</th>
-            </tr> 
-            </thead>
+    <label for>
+      地址
+      <el-input v-model="adress" placeholder="请输入地址" id="adress" style="width:250px"></el-input>
+    </label>
+    <label for="">
+         <el-button type="success" round @click="btn">添加</el-button>
+    </label>
 
-            <tbody>
-            <tr v-for="item in list" :key="item.id">
-                <td>{{item.id}}</td>
-                 <td>{{item.name}}</td>
-                  <td>{{item.age}}</td>
-            </tr> 
-            </tbody>
-          
-          
-        </table>
-    </div>
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180">{{data | time()}}</el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="adress" label="地址"></el-table-column>
+    </el-table>
+  </div>
 </template>
 <script>
 export default {
-    data(){
-        return {
-            h1:"哈哈哈",
-            name:'',
-            age:'',
-            id:'',
-            list:[
-                {
-                    id:"1",
-                    name:"aaa",
-                    age:"18"
-                },
-                  {
-                      id:"2",
-                    name:"bbb",
-                    age:"20"
-                }
-            ]
-        }
-    },
-    methods: {
-        btn(){
-            this.h1 = '孤儿亚索'
+  data() {
+    return {
+      name: "",
+      adress: "",
+      data: new Date(),
+      tableData: [
+        {
+          date: new Date(),
+          name: "zhy",
+          adress: "China"
         },
-        btn1(){
-            var user = {
-            id:this.id,
-            name:this.name,
-            age:this.age
-            }
-            this.list.push(user)
-            this.id=this.name=this.age = ''
-        
+        {
+          date: new Date(),
+          name: "czf",
+          adress: "China"
         }
-    },
+      ]
+    };
+  },
+  methods: {
+   btn(){
+var user = {
+    date:this.date,
+    name:this.name,
+    adress:this.adress
 }
+
+this.tableData.push(user)
+this.date=this.name=this.adress = ''
+      }
+  },
+  filters: {
+    time: function(dateStr, pattren = "") {
+      var dt = new Date(dateStr);
+      console.log(dateStr);
+
+      //yyyy-mm-dd
+      var y = dt.getFullYear();
+      var m = (dt.getMonth() + 1).toString().padStart(2, "0");
+      var d = dt
+        .getDate()
+        .toString()
+        .padStart(2, "0");
+
+      if (pattren.toLowerCase() === 'yyyy-mm-dd"') {
+        return `${y}-${m}-${d}`;
+      } else {
+        var hh = dt
+          .getHours()
+          .toString()
+          .padStart(2, "0"); //padStart用于进行补零操作
+        var mm = dt
+          .getMinutes()
+          .toString()
+          .padStart(2, "0");
+        var ss = dt
+          .getSeconds()
+          .toString()
+          .padStart(2, "0");
+        return `${y}-${m}-${d}  ${hh}:${mm}:${ss}`;
+      }
+    }
+  }
+};
 </script>
 
 <style>
-    #table{
-        margin:0 auto
-    }
+
+
 </style>
